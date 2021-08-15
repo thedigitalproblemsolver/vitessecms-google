437d77dc2945fa77c078504a9753ca0bf1b340ce
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-08-15 21:29:46 +0200

    Added stuff for adsense ads.txt

---
+++ src/Enums/GoogleEnum.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Google\Enums;
+
+use VitesseCms\Core\AbstractEnum;
+
+class GoogleEnum extends AbstractEnum {
+    public const GOOGLE_ADSENSE_ADSTXT = 'GOOGLE_ADSENSE_ADSTXT';
+}
