2e56abbfbb386a7c03fd8dbcef57c672b0f64103
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-07-23 22:27:24 +0200

    added admin exclustion

--- src/Blocks/AdsenseSearch.php
+++ src/Blocks/AdsenseSearch.php
@@ -2,8 +2,17 @@
 
 namespace VitesseCms\Google\Blocks;
 
+use VitesseCms\Admin\Utils\AdminUtil;
 use VitesseCms\Block\AbstractBlockModel;
+use VitesseCms\Block\Models\Block;
 
 class AdsenseSearch extends AbstractBlockModel
 {
+    public function getTemplateParams(Block $block): array
+    {
+        $params = parent::getTemplateParams($block);
+        $params['isAdminPage'] = AdminUtil::isAdminPage();
+
+        return $params;
+    }
 }
